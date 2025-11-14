413e50fc3f43365464cba73e1a2bd14f066dd647
Saeed Halabi <saeedhalabi80@gmail.com>
2025-11-14 19:30:55 +0200

    Add character and ASCII demo.

---
+++ day05/part3/data_types_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char firstChar = 'A';
+    char lastChar = 'Z';
+
+    printf("Character: %c, ASCII value: %d\n", firstChar, firstChar);
+    printf("Character: %c, ASCII value: %d\n", lastChar, lastChar);
+
+    return 0;
+}
