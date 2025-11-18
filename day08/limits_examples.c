27217cd57f14ed71ea8e8389932eca4fafe09020
Saeed Halabi <saeedhalabi80@gmail.com>
2025-11-18 16:26:19 +0200

    add limits.h code demo.

---
+++ day08/limits_examples.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+  printf("Signed short min value: %d\n", SHRT_MIN);
+  printf("Signed short max value: %d\n", SHRT_MAX);
+  printf("Unsigned short max value: %u\n", USHRT_MAX);
+  return 0;
+}
